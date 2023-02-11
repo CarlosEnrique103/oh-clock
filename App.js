fa71cf912fc59bbf75d1ee5c7d406fc888796324
Juan Carlos Echevarria <juancarlosenrique103@gmail.com>
2023-02-11 15:23:13 -0500

    feat: Config rotation and add colors.

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
-import { useCallback } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { useCallback, useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
 import { useFonts } from "expo-font";
-import { StatusBar } from "expo-status-bar";
 import * as SplashScreen from "expo-splash-screen";
+import * as ScreenOrientation from "expo-screen-orientation";
+import colors from "./assets/constants/colors";
 
 export default function App() {
 	const [fontsLoaded] = useFonts({
 		FjallaOne: require("./assets/fonts/FjallaOne.ttf"),
 	});
+
+	const [orientation, setOrientation] = useState(
+		ScreenOrientation.Orientation.Portrait
+	);
 
 	const onLayoutRootView = useCallback(async () => {
 		if (fontsLoaded) {
@@ -15,21 +20,40 @@ export default function App() {
 		}
 	}, [fontsLoaded]);
 
+	useEffect(() => {
+		const unlockOrientation = async () => {
+			await ScreenOrientation.unlockAsync();
+		};
+		unlockOrientation();
+		const subscription = ScreenOrientation.addOrientationChangeListener(
+			(orientationChange) => {
+				setOrientation(orientationChange.orientation);
+			}
+		);
+
+		return () => subscription.remove();
+	}, []);
+
+	useEffect(() => {
+		const getOrientation = async () => {
+			const data = await ScreenOrientation.getOrientationAsync();
+			console.log({ data });
+		};
+
+		getOrientation();
+	}, [orientation]);
+
 	if (!fontsLoaded) {
 		return null;
 	}
-	return (
-		<View style={styles.container} onLayout={onLayoutRootView}>
-			<StatusBar style='auto' />
-			<Text style={{ fontFamily: "FjallaOne", fontSize: 30 }}>Inter Black</Text>
-		</View>
-	);
+
+	return <View style={styles.container} onLayout={onLayoutRootView}></View>;
 }
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: "#fff",
+		backgroundColor: colors.bgApp,
 		alignItems: "center",
 		justifyContent: "center",
 	},
